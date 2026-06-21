6d07795020e6d474288944febf4dec0af9c2b0b7
msohail22 <mohd.sohail.biz@gmail.com>
2026-06-21 22:16:16 +0530

    chore: add pnpm workspace configuration for apps and packages

---
+++ game/src/main.cpp
@@ -0,0 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    if (!glfwInit()) {
+        cout << "fuck chalu nahi ho raha hai\n";
+    }
+    return 0;
+}
