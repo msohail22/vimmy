53176a99ef4a7ab7fc33738f3de24eec208e8c41
msohail22 <mohd.sohail.biz@gmail.com>
2026-06-28 01:58:30 +0530

    single worker config

---
+++ README.md
@@ -0,0 +1,5 @@
+# vimmy
+
+game to learn vim motions effectively
+
+link:  https://vimmy.msohail22.workers.dev
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Hello, Vimmy</h1>
+      <p>a game to learn vim motions in a fun way</p>
+    </>
+  )
+}
+
+export default App
